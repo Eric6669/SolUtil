765007fab13640c59d019193d864e4ae7da5fe9f
Ruizhi Yu <rz.yu@foxmail.com>
2024-12-28 08:56:31 +0800

    feat: temp mdl

--- SolUtil/energyflow/dhs_flow.py
+++ SolUtil/energyflow/dhs_flow.py
@@ -11,6 +11,7 @@ from ..sysparser import load_ngs
 
 __all__ = ["DhsFlow", "mdl_dhs"]
 
+
 class DhsFlow:
 
     def __init__(self,
@@ -18,7 +19,92 @@ class DhsFlow:
         self.hc = load_hs(file)
 
 
+def mdl_temp(hc):
+    """
+    Temperature model based on Solverz, with mass flow as parameters
+    """
+    m = Model()
+    Tamb = hc['Ta']
+    m.m = SolParam('m', hc['m'])
+    m.Ts = SolVar('Ts', hc['Ts'] - Tamb)
+    m.Tr = SolVar('Tr', hc['Tr'] - Tamb)
+    m.Touts = SolVar('Touts', hc['Ts'] - Tamb)
+    m.Toutr = SolVar('Toutr', hc['Tr'] - Tamb)
+    m.min = SolParam('min', 0.1 * np.ones(hc['n_node']))
+    m.Tsource = SolParam('Tsource', hc['Ts'] - Tamb)
+    m.Tload = SolParam('Tload', hc['Tr'] - Tamb)
+    m.lam = SolParam('lam', hc['lam'])
+    m.L = SolParam('L', hc['L'])
+    m.Cp = SolParam('Cp', 4182)
+
+    # Supply temperature
+    for node in range(hc['n_node']):
+        lhs = 0
+        rhs = 0
+
+        if node in hc['s_node'].tolist() + hc['slack_node'].tolist():
+            lhs += Abs(m.min[node])
+            rhs += m.Tsource[node] * Abs(m.min[node])
+
+        for edge in hc['G'].in_edges(node, data=True):
+            pipe = edge[2]['idx']
+            lhs += heaviside(m.m[pipe]) * m.m[pipe]
+            rhs += heaviside(m.m[pipe]) * (m.Touts[pipe] * m.m[pipe])
+
+        for edge in hc['G'].out_edges(node, data=True):
+            pipe = edge[2]['idx']
+            lhs += (1 - heaviside(m.m[pipe])) * m.m[pipe]
+            rhs += (1 - heaviside(m.m[pipe])) * (m.Touts[pipe] * m.m[pipe])
+
+        lhs *= m.Ts[node]
+
+        m.__dict__[f"Ts_{node}"] = Eqn(f"Ts_{node}", lhs - rhs)
+
+    # Return temperature
+    for node in range(hc['n_node']):
+        lhs = 0
+        rhs = 0
+
+        if node in hc['l_node']:
+            lhs += Abs(m.min[node])
+            rhs += m.Tload[node] * Abs(m.min[node])
+
+        for edge in hc['G'].out_edges(node, data=True):
+            pipe = edge[2]['idx']
+            lhs += heaviside(m.m[pipe]) * Abs(m.m[pipe])
+            rhs += heaviside(m.m[pipe]) * (m.Toutr[pipe] * Abs(m.m[pipe]))
+
+        for edge in hc['G'].in_edges(node, data=True):
+            pipe = edge[2]['idx']
+            lhs += (1 - heaviside(m.m[pipe])) * Abs(m.m[pipe])
+            rhs += (1 - heaviside(m.m[pipe])) * (m.Toutr[pipe] * Abs(m.m[pipe]))
+
+        lhs *= m.Tr[node]
+
+        m.__dict__[f"Tr_{node}"] = Eqn(f"Tr_{node}", lhs - rhs)
+
+    # Temperature drop
+    for edge in hc['G'].edges(data=True):
+        fnode = edge[0]
+        tnode = edge[1]
+        pipe = edge[2]['idx']
+        attenuation = exp(- m.lam[pipe] * m.L[pipe] / (m.Cp * Abs(m.m[pipe])))
+        Tstart = m.Ts[fnode] * heaviside(m.m[pipe]) + m.Ts[tnode] * (1 - heaviside(m.m[pipe]))
+        rhs = m.Touts[pipe] - Tstart * attenuation
+        m.__dict__[f"Touts_{pipe}"] = Eqn(f"Touts_{pipe}", rhs)
+        Tstart = m.Tr[tnode] * heaviside(m.m[pipe]) + m.Tr[fnode] * (1 - heaviside(m.m[pipe]))
+        rhs = m.Toutr[pipe] - Tstart * attenuation
+        m.__dict__[f"Toutr_{pipe}"] = Eqn(f"Toutr_{pipe}", rhs)
+
+    temp, y0 = m.create_instance()
+    ntemp, code = made_numerical(temp, y0, sparse=True, output_code=True)
+    return ntemp, y0
+
+
 def mdl_dhs(hc, module_name, jit=True):
+    """
+    Full DHS model using Solverz
+    """
     m = Model()
     Tamb = hc['Ta']
     m.m = SolVar('m', hc['m'])
@@ -26,7 +112,6 @@ def mdl_dhs(hc, module_name, jit=True):
     m.Tr = SolVar('Tr', hc['Tr'] - Tamb)
     m.Touts = SolVar('Touts', hc['Ts'] - Tamb)
     m.Toutr = SolVar('Toutr', hc['Tr'] - Tamb)
-    # m.min = SolVar('min', np.zeros(hc['n_node']))
     m.min = SolVar('min', 0.1 * np.ones(hc['n_node']))
     m.phi_slack = SolVar('phi_slack', np.zeros(1))
     m.Tsource = SolParam('Tsource', hc['Ts'] - Tamb)
@@ -62,7 +147,7 @@ def mdl_dhs(hc, module_name, jit=True):
         lhs = 0
         rhs = 0
 
-        if node in hc['s_node']:
+        if node in hc['s_node'].tolist() + hc['slack_node'].tolist():
             lhs += Abs(m.min[node])
             rhs += m.Tsource[node] * Abs(m.min[node])
 
@@ -123,7 +208,7 @@ def mdl_dhs(hc, module_name, jit=True):
         else:
             phi = m.phi[node]
 
-        if node in hc['s_node']:
+        if node in hc['s_node'].tolist() + hc['slack_node'].tolist():
             rhs = phi - m.Cp / 1e6 * Abs(m.min[node]) * (m.Tsource[node] - m.Tr[node])
         elif node in hc['l_node']:
             rhs = phi - m.Cp / 1e6 * Abs(m.min[node]) * (m.Ts[node] - m.Tload[node])
