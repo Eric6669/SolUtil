205fb95a7a926ea15a7ab4a91402095dc559c427
He Xing <77628887+Eric6669@users.noreply.github.com>
2025-09-23 11:38:54 +0800

    Update __init__.py

--- SolUtil/energyflow/__init__.py
+++ SolUtil/energyflow/__init__.py
@@ -1,5 +1,6 @@
 from .gas_flow import *
 from .dhs_flow import *
+from .dhs_N_1_flow import DhsN_1Flow
 from .dhs_fault_flow import DhsFaultFlow
 from .power_flow import *
 from .hydraulic_mdl import hydraulic_opt_mdl
